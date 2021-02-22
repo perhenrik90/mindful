4e958aec026c8a68d11b4aba26fcc6b8aa11f966
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2021-02-22 18:57:19 +0100

    started first match statement for route first argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+
 fn main() {
+
+    let first_arg = std::env::args().nth(1).expect("help");
     println!("Hello, world!");
+
+
+    match & first_arg[..]{
+
+	"in" => println!("Check inn..."),
+	"help" => println!("Print help text"),
+
+	_ => println!("No argument given")
+    }
+
+
+
+
 }
