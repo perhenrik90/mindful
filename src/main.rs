311735f17026a6fa9f3dec2a8324f2dc023c85e6
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2021-02-28 00:50:49 +0100

    added printout from checkout

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,13 @@ use std::fs;
 use std::env;
 use std::io::Write;
 use std::ffi::OsStr;
-
 use chrono::{DateTime, Utc, NaiveDateTime, Local};
 
+/****************************************************************
+ * MINDFUL
+ *
+ * @author Per-Henrik E.K | 2021
+ ****************************************************************/
 
 fn get_config(option: &str) -> String{
     let mut  config :String =  env::var_os("HOME").expect("Error in env").into_string().expect("Could not parese to string");
@@ -29,14 +33,20 @@ fn check_in() {
     let now = Local::now();
     let filepath = get_config("in");
 
-    println!("{}", filepath);
+    // probably make an alert messages before overwriting checkin timestamp ? 
     let mut f = File::create(filepath).expect("Unable to open");
     f.write( &now.to_rfc3339().as_bytes());
 }
 
 fn out() {
+    /**
+     * Checking mindful out
+     **/
     println!("Checking out");
+    let filepath = get_config("in");
+    let inn_content = fs::read_to_string(filepath).expect("Can not open in timestamp");
 
+    println!("Inn timestamp {}", inn_content);
     
 }
 fn main() {
